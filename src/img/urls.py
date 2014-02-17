9e882e07cb294b594474b51fe208ccf052d28879
Thibault Jouannic <thibault@miximum.fr>
2014-02-17 14:19:28 +0100

    Correct url include

--- src/img/urls.py
+++ src/img/urls.py
@@ -3,10 +3,8 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'img.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+urlpatterns = patterns(
+    '',
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include('images.urls')),
 )
