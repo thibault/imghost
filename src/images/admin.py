9dd76d9d34ad86cad6748ad970a81009091b0017
Thibault Jouannic <thibault@miximum.fr>
2014-02-21 07:38:51 +0100

    Admin action: regenerate thumbnails

--- src/images/admin.py
+++ src/images/admin.py
@@ -1,10 +1,18 @@
 from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
 
 from images.models import Image
 
 
 class ImageAdmin(admin.ModelAdmin):
     list_display = ('unique_key', 'image', 'extension')
+    actions = ['make_thumbnails']
+
+    def make_thumbnails(self, request, queryset):
+        for img in queryset:
+            img.generate_thumbnails()
+    make_thumbnails.short_description = _('Regenerates thumbnails')
+
 
 
 admin.site.register(Image, ImageAdmin)
