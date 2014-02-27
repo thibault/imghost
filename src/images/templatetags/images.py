e332ddd4d2cf980319d04be62962d43370575bb1
Thibault Jouannic <thibault@miximum.fr>
2014-02-27 10:11:26 +0100

    Remove /media/ in image files urls

--- src/images/templatetags/images.py
+++ src/images/templatetags/images.py
@@ -5,6 +5,7 @@ register = template.Library()
 
 
 @register.simple_tag(takes_context=True)
-def full_url(context, url):
+def full_img_url(context, url):
     request = context['request']
-    return request.build_absolute_uri(url)
+    clean_url = url.replace('/media/i/', '/i/')
+    return request.build_absolute_uri(clean_url)
