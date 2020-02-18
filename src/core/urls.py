8ab5030af8476d014c60f1c17c04fcc5d8be5d2b
Thibault Jouannic <thibault@miximum.fr>
2020-02-18 17:12:44 +0100

    Update some stuff

--- src/core/urls.py
+++ src/core/urls.py
@@ -13,3 +13,10 @@ urlpatterns = [
     path('images/', include('images.urls')),
     path('memes/', include('memes.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path(r'__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
